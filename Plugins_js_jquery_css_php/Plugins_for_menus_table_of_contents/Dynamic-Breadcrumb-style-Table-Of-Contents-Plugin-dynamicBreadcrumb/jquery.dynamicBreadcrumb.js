a0c86dbe0541052ffb20eab779e93a5da1511328
local <up778@users.noreply.github.com>
2025-06-25 06:53:20 +0200

    ---

--- Plugins_js_jquery_css_php/Plugins_for_menus_table_of_contents/Dynamic-Breadcrumb-style-Table-Of-Contents-Plugin-dynamicBreadcrumb/jquery.dynamicBreadcrumb.js
+++ Plugins_js_jquery_css_php/Plugins_for_menus_table_of_contents/Dynamic-Breadcrumb-style-Table-Of-Contents-Plugin-dynamicBreadcrumb/jquery.dynamicBreadcrumb.js
@@ -82,7 +82,7 @@ $("head").append(styleTag);
           level = i;
 
           currentContainer = $(
-            "." + settings.levelClassPrefix + i + ":in-viewport( 120 )",
+            "." + settings.levelClassPrefix + i + ":in-viewport( 300 )",
           ).first();
 
           if (
