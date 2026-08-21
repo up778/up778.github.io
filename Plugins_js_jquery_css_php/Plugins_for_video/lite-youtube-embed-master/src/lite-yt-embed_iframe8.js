81fbe62761d36b4b62cfb61dd8adfd7b1e0187c2
local <71689178+flower-spring@users.noreply.github.com>
2026-08-21 10:55:11 +0200

    Auto-save commit for lite-yt-embed_iframe8.js

--- Plugins_js_jquery_css_php/Plugins_for_video/lite-youtube-embed-master/src/lite-yt-embed_iframe8.js
+++ Plugins_js_jquery_css_php/Plugins_for_video/lite-youtube-embed-master/src/lite-yt-embed_iframe8.js
@@ -374,7 +374,7 @@ function onPlayerStateChange(event) {
     //   event.target.g?.closest("lite-youtube") || null;
 
 
-    const iframe = event.target.getIframe();
+    iframe = event.target.getIframe();
     dernier_element_lite_yt_joué = iframe?.closest("lite-youtube") || null;
 
     handle_reset_if_single_visible();
